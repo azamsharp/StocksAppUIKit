ada95d030a8d900f493538327c0ecc5fd22b8cc8
Mohammad Azam <azamsharp@gmail.com>
2022-01-20 12:10:37 -0600

    Initial Commit

---
+++ StocksAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksAppUIKit
+//
+//  Created by Mohammad Azam on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
